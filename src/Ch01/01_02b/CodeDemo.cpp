a5b7cac31cd16e7afe1239b29b2bcb079853a472
claire-wadman <claire.wadman@aero.org>
2025-10-30 18:35:59 +0000

    Added comments

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,47 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+/* # indicates compiler directive
+   following line tells compiler to include header file iostream (library) */
+#include <iostream> // preprocessor directive
+
+// every line of executable code must be a part of a function
+
+// must have main function that returns int
+int main(){
+    // :: = scope resolution operator
+    // << = insertion operator
+    std::cout << "Hello World" << std::endl;
+    std::cout << std::endl << std::endl;
+    return 0;
+
+}
+
+/*
+Alternate hello worlds :
+---------------------------------------------
+#include <cstdio>
+
+int main(){
+    printf("Hello World\n");
+
+    return 0;
+}
+---------------------------------------------
+
+#include <iostream>
+// command line program that produces an executable
+
+int main(int argc, char* argv[]){
+    // argc = integer that represents the number of command line arguments
+    // passed to the program
+
+    // argv = array of c-style strings representing those arguments
+
+    std::cout << "Hello world" << std::endl;
+    return 0;
+
+}
+
+*/
