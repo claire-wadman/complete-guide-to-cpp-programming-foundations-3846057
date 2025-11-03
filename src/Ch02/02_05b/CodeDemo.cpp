7746241ec7d216b88ba7757e56e37b33f704b65a
claire-wadman <claire.wadman@aero.org>
2025-11-03 21:29:28 +0000

    Completed exercise

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -4,8 +4,42 @@
 
 #include <iostream>
 
+int a,b = 5; // a and b are global variables
+
+/* Global variables:
+Accessible to all parts of code;
+Memory managed statically by compiler;
+Allocated in sata segment of memory;
+Memory freed once program ends;
+Best practice to avoid global vars
+
+Automatic variables:
+Local to their scope;
+Defined within functions/loops;
+Accessible in their scope and scopes inside of it only;
+Once execution exits scope, variables deleted and memory freed;
+Managed by compiler;
+Allocated in stack segment of memory (temporary)
+*/
+
+
 int main(){
-    
+    bool my_flag;
+    a = 7;
+    my_flag = false;
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    std::cout << "flag = " << my_flag << std::endl;
+    my_flag = true;
+    std::cout << "flag = " << my_flag << std::endl;
+    std::cout << "a + b = " << a + b << std::endl;
+    std::cout << "b - a = " << b - a << std::endl;
+    unsigned int positive; //can make declarations anywhere in code
+    positive = b - a; // result is 2^32 - 2 (two's complement representation of -2 in binary)
+    std::cout << "b - a (unsigned) = " << positive << std::endl;
+
+
     std::cout << std::endl << std::endl;
+
     return 0;
 }
